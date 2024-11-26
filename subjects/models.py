8b052f9a461cbe12fcec340e17f060e5a80b649c
Chugani05 <chugani205@gmail.com>
2024-11-26 20:27:06 +0000

    Se termina de implementar los modelos

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    code = models.CharField(unique=True, max_length=3)
+    name = models.CharField(max_length=255)
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='subjects'
+    )
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, through='users.Enrollment', related_name='subjects'
+    )
+
+
+class Lesson(models.Model):
+    subject = models.OneToOneField('subjects.Subject', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    STUDENT = 'S'
+    TEACHER = 'T'
+    ROLES_CHOICES = {STUDENT: 'Student', TEACHER: 'Teacher'}
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=2, choices=ROLES_CHOICES, default=STUDENT)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(
+        blank=True, null=True, upload_to='avatars', default='avatars/noavatar.png'
+    )
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrollments'
+    )
+    subject = models.ForeignKey(
+        'subjects.Subject', on_delete=models.CASCADE, related_name='enrollments'
+    )
+    enrolled_at = models.DateField(auto_now_add=True)
+    mark = models.IntegerField(null=True)
