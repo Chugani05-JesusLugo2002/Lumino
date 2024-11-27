fe269de58df60494a47f4158314bc40e6d016224
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-27 18:12:10 +0000

    Se termina de implementar los modelos

--- subjects/models.py
+++ subjects/models.py
@@ -6,14 +6,24 @@ class Subject(models.Model):
     code = models.CharField(unique=True, max_length=3)
     name = models.CharField(max_length=255)
     teacher = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='subjects'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacher_subjects'
     )
     students = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, through='users.Enrollment', related_name='subjects'
+        settings.AUTH_USER_MODEL,
+        through='users.Enrollment',
+        related_name='student_subjects',
     )
+
+    def __str__(self) -> str:
+        return f'{self.name} ({self.code}), imparted by {self.teacher}'
 
 
 class Lesson(models.Model):
-    subject = models.OneToOneField('subjects.Subject', on_delete=models.CASCADE)
+    subject = models.ForeignKey(
+        'subjects.Subject', on_delete=models.CASCADE, related_name='lessons'
+    )
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
--- users/models.py
+++ users/models.py
@@ -3,24 +3,30 @@ from django.db import models
 
 
 class Profile(models.Model):
-    STUDENT = 'S'
-    TEACHER = 'T'
-    ROLES_CHOICES = {STUDENT: 'Student', TEACHER: 'Teacher'}
+    class Role(models.TextChoices):
+        STUDENT = 'S', 'Student'
+        TEACHER = 'T', 'Teacher'
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    role = models.CharField(max_length=2, choices=ROLES_CHOICES, default=STUDENT)
-    bio = models.TextField(blank=True)
+    role = models.CharField(max_length=1, choices=Role, default=Role.STUDENT)
+    bio = models.TextField(blank=True, null=True)
     avatar = models.ImageField(
         blank=True, null=True, upload_to='avatars', default='avatars/noavatar.png'
     )
 
+    def __str__(self) -> str:
+        return f'{self.role}: {self.user}'
+
 
 class Enrollment(models.Model):
     student = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrollments'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='student_enrollments'
     )
     subject = models.ForeignKey(
-        'subjects.Subject', on_delete=models.CASCADE, related_name='enrollments'
+        'subjects.Subject', on_delete=models.CASCADE, related_name='subject_enrollments'
     )
     enrolled_at = models.DateField(auto_now_add=True)
     mark = models.IntegerField(null=True)
+
+    def __str__(self) -> str:
+        return f'{self.student}, enrolled at {self.enrolled_at} in {self.subject}'
