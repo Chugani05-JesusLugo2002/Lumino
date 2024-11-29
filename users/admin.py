4fc57b70d748ea81a71009ae769417cc2f83d202
Chugani05 <chugani205@gmail.com>
2024-11-29 16:32:01 +0000

    se arregla admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, Enrollment
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
     pass
