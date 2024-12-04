7ab55326a5c3ab65811e89a34d1f153a8bcb58d2
Chugani05 <chugani205@gmail.com>
2024-12-04 16:21:00 +0000

    Se adelanta views de users

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Profile
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = (
+            'avatar',
+            'bio',
+        )
--- users/views.py
+++ users/views.py
@@ -1,17 +1,37 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
+from django.shortcuts import redirect, render
+
+from .forms import EditProfileForm
 
 
+@login_required
 def user_detail(request: HttpRequest, username: str) -> HttpResponse:
-    pass
+    consumer = User.objects.get(username=username)
+    return render(request, 'users/user_detail.html', dict(consumer=consumer))
 
 
-def edit_profile(request: HttpRequest) -> HttpResponse:
-    pass
+@login_required
+def edit_profile(request: HttpRequest, username: str) -> HttpResponse:
+    user = User.objects.get(username=username)
+    user_profile = user.profile
+    if request.method == 'POST':
+        if (form := EditProfileForm(request.POST, request.FILES, instance=user_profile)).is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('users:user-detail', username)
+    else:
+        form = EditProfileForm(instance=user_profile)
+    return render(request, 'users/edit_profile.html', dict(form=form))
 
 
+@login_required
 def request_certificate(request: HttpRequest) -> HttpResponse:
     pass
 
 
+@login_required
 def leave(request: HttpRequest) -> HttpResponse | HttpResponseForbidden:
     pass
