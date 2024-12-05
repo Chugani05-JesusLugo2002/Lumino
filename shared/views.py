b7d43f7d03d47f63f447c38f6b4008bcc16f56fe
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-12-05 18:41:01 +0000

    Progress on shared templates

--- shared/views.py
+++ shared/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import redirect, render
 def index(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
         return redirect('subjects:subject-list')
-    else:
-        return render(request, 'index.html')
+    return render(request, 'index.html')
