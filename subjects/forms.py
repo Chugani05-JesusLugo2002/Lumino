61e05061e326b5b5f95dee2b745a08dd15fa2e58
Chugani05 <chugani205@gmail.com>
2024-12-10 19:34:17 +0000

    algo

--- subjects/forms.py
+++ subjects/forms.py
@@ -1,7 +1,8 @@
+# from crispy_forms.layout import HTML, Field, Layout, Submit
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Lesson, Enrollment
+from .models import Enrollment, Lesson
 
 
 class AddLessonForm(forms.ModelForm):
@@ -12,6 +13,17 @@ class AddLessonForm(forms.ModelForm):
     def __init__(self, subject, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.subject = subject
+        # self.helper.layout = Layout(
+        #     Field('title'),
+        #     Field('content'),
+        #     FormActions(
+        #         Submit('add', 'Add', css_class='btn btn-danger btn-lg'),
+        #         HTML(
+        #             '<a class="btn btn-danger btn-lg" href="{{ subject.get_absolute_url }}"><i class="bi bi-arrow-left-circle"></i> Cancel</a>'
+        #         ),
+        #     ),
+        # )
+    
 
     def save(self, *args, **kwargs):
         lesson = super().save(commit=False)
@@ -28,16 +40,15 @@ class EditLessonForm(forms.ModelForm):
             'content',
         )
 
+
 class EditMarkForm(forms.ModelForm):
     class Meta:
         model = Enrollment
-        fields = 'mark',
-        widgets = {
-            'mark': forms.TextInput(attrs={'size': 3})
-        }
-    
+        fields = ('mark',)
+        widgets = {'mark': forms.TextInput(attrs={'size': 3})}
+
     def clean_mark(self):
-        mark = self.cleaned_data["mark"]
+        mark = self.cleaned_data['mark']
         if mark:
             try:
                 mark = int(mark)
@@ -46,4 +57,3 @@ class EditMarkForm(forms.ModelForm):
             if not 0 <= mark <= 10:
                 raise ValidationError('Mark is not between the allowed range (0-10)!')
         return mark
-    
