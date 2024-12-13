fe87df107285e1955a3ec15269aa3010a786d325
Chugani05 <chugani205@gmail.com>
2024-12-13 16:56:17 +0000

    Progress on accounts forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,9 @@
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import HTML, Div, Layout
 from django import forms
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from users.models import Profile
 
@@ -11,6 +15,25 @@ class LoginForm(forms.Form):
     def get_credentials(self):
         return self.cleaned_data['username'], self.cleaned_data['password']
 
+    def __init__(self, subject, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.subject = subject
+        self.helper = FormHelper()
+        self.helper.form_class = 'needs-validation'
+        self.helper.layout = Layout(
+            FloatingField('username'),
+            FloatingField('password'),
+            Div(
+                HTML('<button type="submit" class="btn btn-primary w-100 mt-4">Login</button>'),
+            ),
+        )
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        if len(data) <= 0:
+            return ValidationError('¿No tienes nombre?')
+        return data
+
 
 class SignupForm(forms.ModelForm):
     class Meta:
@@ -18,6 +41,22 @@ class SignupForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
         widgets = dict(password=forms.PasswordInput)
         help_texts = dict(username=None)
+
+    def __init__(self, subject, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.subject = subject
+        self.helper = FormHelper()
+        self.helper.form_class = ''
+        self.helper.layout = Layout(
+            FloatingField('first_name'),
+            FloatingField('last_name'),
+            FloatingField('username'),
+            FloatingField('email'),
+            FloatingField('password'),
+            Div(
+                HTML('<button type="submit" class="btn btn-primary w-100 mt-4">Signup</button>'),
+            ),
+        )
 
     def save(self, *args, **kwargs):
         user = super().save(commit=False)
