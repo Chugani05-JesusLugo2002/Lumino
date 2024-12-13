ff98194f29517a57d31ef6ed4445a42ce2242c64
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-12-13 10:45:08 +0000

    Progress on refactor subject_detail (modal pending); add alert custom templatetag

--- shared/templatetags/shared_extras.py
+++ shared/templatetags/shared_extras.py
@@ -7,3 +7,7 @@ register = template.Library()
 def modal(button_label: str = 'Action', title: str = 'Action', body: str = 'Are you sure?', url: str = '', css_class: str = 'btn btn-primary', modal_button_label: str = 'Action'):
     modal_id = uuid4()
     return dict(modal_id=modal_id, title=title, body=body, url=url, button_label=button_label, css_class=css_class, modal_button_label=modal_button_label)
+
+@register.inclusion_tag('components/alert.html')
+def alert(text: str):
+    return dict(text=text)
