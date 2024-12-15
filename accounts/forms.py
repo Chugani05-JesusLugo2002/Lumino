025d5983e4ccd8a27e8f718005628e08acc77457
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-12-15 11:50:22 +0000

    Add crispy helper in login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,27 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, HTML
+from crispy_bootstrap5.bootstrap5 import FloatingField
 
-from users.models import Profile
+from users.models import Profile    
 
 
 class LoginForm(forms.Form):
     username = forms.SlugField(max_length=64, required=True)
     password = forms.CharField(max_length=64, required=True, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            HTML('<h3 class="card-title text-center mb-4">Login</h3>'),
+            FloatingField('username'),
+            FloatingField('password'),
+            Submit('login', 'Login', css_class="btn btn-primary btn-block w-100 mt-3"),
+            HTML('<p class="text-center mt-4">No account? <a href="{% url "signup" %}" class="link-primary text-decoration-none">Create one!</a></p>')
+        )
 
     def get_credentials(self):
         return self.cleaned_data['username'], self.cleaned_data['password']
